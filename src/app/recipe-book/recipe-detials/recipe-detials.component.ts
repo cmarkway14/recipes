d3b1ee2cc5d3c90f705543c58c76a700536c21ae
Markway <markwayc@stifel.com>
2019-11-04 14:42:56 -0600

    added stepper

--- src/app/recipe-book/recipe-detials/recipe-detials.component.ts
+++ src/app/recipe-book/recipe-detials/recipe-detials.component.ts
@@ -1,6 +1,6 @@
 import { Recipe } from 'src/app/Models/Recipe';
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-recipe-detials',
@@ -10,10 +10,14 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 export class RecipeDetialsComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public recipe: Recipe) { 
-    
+
   }
 
   ngOnInit() {
   }
 
+  public Next(stepper : MatStepper) : void{
+    console.log(stepper._selectedIndex)
+    stepper.next();
+  }
 }
